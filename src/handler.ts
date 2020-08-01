b3bbad0190b9c0e2fc732a7e04ad9b1ba1dbc2f8
Evans A. Stepanov <evans@stepanov.email>
2020-08-01 11:47:16 -0400

    initial commit

---
+++ src/handler.ts
@@ -0,0 +1,23 @@
+import { APIGatewayProxyHandler, APIGatewayEvent, Context, Callback } from 'aws-lambda';
+import 'source-map-support/register';
+import { echo } from "@queries/exampleQuery";
+
+export const hello: APIGatewayProxyHandler = async (
+  event: APIGatewayEvent,
+  context: Context,
+  callback: Callback,
+): Promise<any> => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: echo('Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!'),
+        input: event,
+        context: context,
+        callback: callback,
+      },
+      null,
+      2,
+    ),
+  };
+};
